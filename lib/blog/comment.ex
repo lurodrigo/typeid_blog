6b350aea0d10a99a0e0757f2487b4800bdcbb4c9
Luiz Rodrigo de Souza <me@lurodrigo.com>
2024-10-31 13:32:24 -0300

    works with binary_id

--- lib/blog/comment.ex
+++ lib/blog/comment.ex
@@ -2,10 +2,10 @@ defmodule Blog.Comment do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @primary_key {:id, TypeID, autogenerate: true, prefix: "comment", type: :uuid}
-  @foreign_key_type TypeID
-  # @primary_key {:id, :binary_id, autogenerate: true}
-  # @foreign_key_type :binary_id
+  # @primary_key {:id, TypeID, autogenerate: true, prefix: "comment", type: :uuid}
+  # @foreign_key_type TypeID
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
   schema "comments" do
     field :body, :string
     belongs_to :post, Blog.Post
